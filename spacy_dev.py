93662e3f51a5c2566f1564040683fed26c298c11
CodeC <58306894+CodeCLS@users.noreply.github.com>
2025-10-17 23:20:45 +0200

    spacy testing and practising

---
+++ spacy_dev.py
@@ -0,0 +1,8 @@
+from __future__ import annotations
+import itertools
+from collections import defaultdict
+from typing import List, Dict, Set, Tuple
+
+import spacy
+import networkx as nx
+import matplotlib.pyplot as plt
