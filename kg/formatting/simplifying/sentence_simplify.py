0bcf2505005c77d20fe931adea2aab1109c3e2e8
CodeC <58306894+CodeCLS@users.noreply.github.com>
2025-11-06 17:47:29 +0100

    embedding AMR into the architecture to sythesize sentences

--- kg/formatting/simplifying/sentence_simplify.py
+++ kg/formatting/simplifying/sentence_simplify.py
@@ -1,21 +1,12 @@
 # kg/formatting/simplifying/sentence_simplifier.py
-from kg.formatting.simplifying.sentence_simplify_agent import SentenceSimplifyAgent
-
+import spacy
 
 class SentenceSimplifier:
     def __init__(self):
-        self.agent = SentenceSimplifyAgent(
-           "You rewrite the following SINGLE sentence into a simpler WORDED sentence.\n"
-            "Rules (NO information loss):\n"
-            "- Keep ALL facts, conditions, constraints, references, and entities.\n"
-            "- You may simplify wording and grammar and simplify sentences if the content and information stays the same and there is no data loss.\n"
-            "- Do NOT remove or change numbers, measurement units, ranges, or limits.\n"
-            "- Do NOT rename technical terms; keep domain terms verbatim.\n"
-            "- Preserve quoted phrases, bracketed tags, and parenthetical content.\n"
-            "- Output MUST be exactly ONE sentence.\n"
-        )
+        self.nlp = spacy.load("en_core_web_sm")
+
 
     def run(self, sentence: str):
-        print("Run sentence simplifier")
-        result = self.agent.run(sentence)
-        return result
+        doc = self.nlp(sentence) 
+        lemmas = [token.lemma_ for token in doc]
+        return lemmas
